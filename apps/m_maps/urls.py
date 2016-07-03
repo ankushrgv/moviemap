5a0bb0e9ab720a0094c6f659be0f4628127541cc
ankushrgv <ankush.rgv@gmail.com>
2016-07-03 12:11:12 +0530

    Basic template render of San Francisco map

--- apps/m_maps/urls.py
+++ apps/m_maps/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    # url(r'^home/$',views.Index.as_view(), name='index'),
+    url(r'^home/$',views.Index.as_view(), name='index'),
     
 ]
--- apps/m_maps/views.py
+++ apps/m_maps/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from apps.m_maps import models
+
+
+class Index(TemplateView):
+
+	template_name = 'm_maps/index.html'
+
+	def get_context_data(self, **kwargs):
+
+		context = super(Index, self).get_context_data(**kwargs)
+		return context
